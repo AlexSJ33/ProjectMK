99bce9767ff8541c50e0de9a18c939633a8eff9c
AlexSJ33 <alevildark@gmail.com>
2023-08-16 18:02:56 -0300

    Manipulando DOM

--- public/index.js
+++ public/index.js
@@ -1,5 +1,28 @@
+
+
 const version_mk = document.getElementById('version_mk')
 const personagem = document.getElementById('personagem')
+const selectVersion = document.getElementById("version_mk")
+const selectPersonagem = document.getElementById("personagem")
+
+
+async function fetchDataBase() {
+  const response = await fetch('http://localhost:3000/versao')
+  const info = await response.json()
+  //const versao = info[1]["name"]
+  //console.log(info)
+  //console.log(versao)
+  info.forEach(function (versao) {
+    selectVersion.appendChild(new Option(versao['name']))
+    
+  });
+
+  }
+  
+fetchDataBase()
+
+
+
 
 version_mk.addEventListener('change', function(){
     console.log(version_mk.value)
